523c190c416d33e5e44a47a6ca3922ca096bf663
yajradevs <rmxmis.arjay@gmail.com>
2026-07-03 11:45:05 +0800

    Update bootstrap_metrics.ps1

--- bootstrap_metrics.ps1
+++ bootstrap_metrics.ps1
@@ -9,7 +9,7 @@ if (!(Test-Path $BaseDir)) {
 if (!(Get-Command python.exe -ErrorAction SilentlyContinue)) {
 
     Invoke-WebRequest `
-        -Uri "https://github.com/USERNAME/wazuh-python-monitoring/releases/download/v1/python-3.12.0-amd64.exe" `
+        -Uri "https://github.com/yajradevs/wazuh-python-monitoring/releases/download/v1/python-3.12.0-amd64.exe" `
         -OutFile $PythonInstaller
 
     Start-Process `
@@ -27,9 +27,9 @@ python -m pip install speedtest-cli
 
 # Download scripts
 Invoke-WebRequest `
-    -Uri "https://raw.githubusercontent.com/USERNAME/wazuh-python-monitoring/main/cpu_ram_metrics.py" `
+    -Uri "https://raw.githubusercontent.com/yajradevs/wazuh-python-monitoring/main/cpu_ram_metrics.py" `
     -OutFile "$BaseDir\cpu_ram_metrics.py"
 
 Invoke-WebRequest `
-    -Uri "https://raw.githubusercontent.com/USERNAME/wazuh-python-monitoring/main/internet_speed.py" `
+    -Uri "https://raw.githubusercontent.com/yajradevs/wazuh-python-monitoring/main/internet_speed.py" `
     -OutFile "$BaseDir\internet_speed.py"
